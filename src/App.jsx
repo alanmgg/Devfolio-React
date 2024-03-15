ce80f31665455dd7ab5352d158a7d5e56a4ac2b4
Alan Francisco Mora <alanfmorag@gmail.com>
2024-03-14 19:18:55 -0600

    :bulb: Project created

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div class="w-screen h-screen bg-gradient-to-t from-gray-50 to-gray-300">
+        <button class="mx-5 my-8 p-2 hover:bg-slate-400">Noche</button>
+
+        <header class="container mx-auto px-[224px] font-open-sans">
+          <h1 class="font-medium text-3xl">Alan Francisco Mora</h1>
+        </header>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      "open-sans": ["Open Sans", "sans-serif"],
+      "noto-sans": ["Noto Sans", "sans-serif"]
+    },
+    extend: {}
+  },
+  plugins: []
+};
